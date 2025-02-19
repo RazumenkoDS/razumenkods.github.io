f7439e100f43a713890dc0dedbfbca8651f64eff
RazumenkoDS <dslycurgus@gmail.com>
2025-02-19 16:38:52 +0300

    generated static HTML via flask
    Flask has successfully generated the static HTML file for github

--- flaskapp.py
+++ flaskapp.py
@@ -2,10 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
-# Route to serve the profile page
 @app.route('/')
 def home():
-    return render_template('index.html')
+    # Render the HTML content dynamically
+    html_content = render_template('index.html')  # This will get your template content
+
+    # Write the rendered HTML to a file
+    with open('index.html', 'w') as file:
+        file.write(html_content)
+
+    print("Static HTML file has been saved!")
+    return html_content  # Optional: return content to see in the browser
 
 if __name__ == '__main__':
     app.run(debug=True)
